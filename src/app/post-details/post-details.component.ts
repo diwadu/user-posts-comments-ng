f0dea71b589aa007479a1f6cff3e9bebc16e686c
Dawid Chrzanowski <dawid.chrzanowski@novacura.com>
2019-07-26 17:15:26 +0200

    uni tets fixes-doesnt work still, but it's better

--- src/app/post-details/post-details.component.ts
+++ src/app/post-details/post-details.component.ts
@@ -1,41 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PostModel } from '../common/models/post.model';
-import { PostDetailsService } from './post-details.service';
-import { CommentModel } from '../common/models/comment.model';
-import { forkJoin } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PostModel } from "../common/models/post.model";
+import { PostDetailsService } from "./post-details.service";
+import { CommentModel } from "../common/models/comment.model";
+import { forkJoin } from "rxjs";
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-post-details',
-  templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.scss']
+  selector: "app-post-details",
+  templateUrl: "./post-details.component.html",
+  styleUrls: ["./post-details.component.scss"]
 })
 export class PostDetailsComponent implements OnInit {
-
   public post: PostModel;
   public comments: CommentModel[];
   public currentCommentId: number;
-  public formOpened: boolean = false;
+  public formOpened = false;
   public newCommentForm: FormGroup;
-  public submitted: boolean = false;
+  public submitted = false;
   private postId: number;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private postDetailsService: PostDetailsService,
-    private formBuilder: FormBuilder) { }
-
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.postId = this.activatedRoute.snapshot.params['postId'];
+    this.postId = this.activatedRoute.snapshot.params["postId"];
     this.getData();
     this.newCommentForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      body: ['', [Validators.required, Validators.minLength(20)]]
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      body: ["", [Validators.required, Validators.minLength(20)]]
     });
   }
 
@@ -48,21 +46,24 @@ export class PostDetailsComponent implements OnInit {
     if (this.newCommentForm.invalid) {
       return;
     }
-    let comment = new CommentModel(0,
+    let comment = new CommentModel(
+      0,
       this.postId,
-      this.newCommentForm.get('name').value,
-      this.newCommentForm.get('email').value,
-      this.newCommentForm.get('body').value
+      this.newCommentForm.get("name").value,
+      this.newCommentForm.get("email").value,
+      this.newCommentForm.get("body").value
     );
-    this.postDetailsService.createComment(comment).subscribe(val => {
-      $('#alertCommentCreated').show();
-    }, (err) => {
-      console.error(err);
-    });
+    this.postDetailsService.createComment(comment).subscribe(
+      val => {
+        $("#alertCommentCreated").show();
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
   getData() {
-
     const post$ = this.postDetailsService.getPostById(this.postId);
     const comments$ = this.postDetailsService.getPostComments(this.postId);
 
@@ -78,16 +79,16 @@ export class PostDetailsComponent implements OnInit {
   }
 
   deleteComment() {
-    this.postDetailsService.deleteComment(this.currentCommentId).subscribe(data => {
-      console.log(data);
-      this.getData();
-      jQuery('#deleteCommentModal').modal('hide');
-
-    });
+    this.postDetailsService
+      .deleteComment(this.currentCommentId)
+      .subscribe(data => {
+        console.log(data);
+        this.getData();
+        jQuery("#deleteCommentModal").modal("hide");
+      });
   }
 
   openCloseForm(): void {
     this.formOpened = !this.formOpened;
   }
-
 }
