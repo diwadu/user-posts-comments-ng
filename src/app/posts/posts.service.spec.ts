fd93a316bc529d65e56dcd40fdfc39805818b8ca
diwadu <diwadu@gmail.com>
2019-07-30 18:41:07 +0200

    Unit test fixing

--- src/app/posts/posts.service.spec.ts
+++ src/app/posts/posts.service.spec.ts
@@ -2,14 +2,53 @@ import { TestBed } from "@angular/core/testing";
 
 import { PostsService } from "./posts.service";
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { UserModel } from '../common/models/user.model';
+//import 'jasmine'
 
+let service: PostsService;
+let httpTestingController: HttpTestingController;
 describe("PostsService", () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
+    imports: [HttpClientTestingModule]
   }));
 
-  it("should be created", () => {
-    const service: PostsService = TestBed.get(PostsService);
-    expect(service).toBeTruthy();
+  beforeEach(async () => {
+    service = TestBed.get(PostsService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
+
+  it("GetUsers should map server response to UserModel array", () => {
+    spyOn(service, 'getUsers').and.callThrough();
+    service.getUsers().subscribe((users: UserModel[]) => {
+      const user = users[0];
+      expect(user.name).toEqual("John Doe");
+      expect(user).toEqual(jasmine.objectContaining({
+        id: 1,
+        name: "John Doe"
+      }));
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/users`);
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush([{
+      "id": 1,
+      "name": "John Doe",
+
+    }]);
+
+
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
 });
+
